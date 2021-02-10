ba9e62a51050d0aa06bc8ef7c4b780d1bf8bf2d7
cweiting19 <cweiting.19@ichat.sp.edu.sg>
2021-02-11 01:14:01 +0800

    updated washes

--- washes.js
+++ washes.js
@@ -14,43 +14,34 @@ bot.on('message', message => {
 
     if (command === 'washes') {
         if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments! $washes <job> <level> <mp>`);
+            return message.channel.send(`You didn't provide any arguments! for belbel: $washes <job> <level> <mp>`);
         }
         else if (args[0] === 'spearman') {
             message.channel.send('spearman wash entered!');
         }
-
-        message.channel.send(`Command namne: ${command}\nArguments: ${args}\nFirst argument: ${args[0]}`)
+        else if (args[0] === 'fighter') {
+            message.channel.send('fighter wash entered!');
+        }
+        else if (args[0] === 'page') {
+            message.channel.send('page wash entered!');
+        }
+        else if (args[0] === 'archer') {
+            message.channel.send('archer wash entered!');
+        }
+        else if (args[0] === 'thief') {
+            message.channel.send('thief wash entered!');
+        }
+        else if (args[0] === 'brawler') {
+            message.channel.send('brawler wash entered!');
+        }
+        else if (args[0] === 'gunslinger') {
+            message.channel.send('gunslinger wash entered!');
+        }
+        else if (args[0] === 'magician') {
+            message.channel.send('magician wash entered!');
+        }
     }
 
 })
-/*switch (args[0]) {
-    case 'washes':
-        if (args[1].toLowerCase() === 'spearman') {
-            message.channel.send('spearman wash');
-        }
-        else if (args[1].toLowerCase() === 'fighter') {
-            message.channel.send('fighter wash');
-        }
-        else if (args[1].toLowerCase() === 'page') {
-            message.channel.send('page wash');
-        }
-        else if (args[1].toLowerCase() === 'archer') {
-            message.channel.send('archer wash');
-        }
-        else if (args[1].toLowerCase() === 'thief') {
-            message.channel.send('thief wash');
-        }
-        else if (args[1].toLowerCase() === 'brawler') {
-            message.channel.send('brawler wash');
-        }
-        else if (args[1].toLowerCase() === 'gunslinger') {
-            message.channel.send('gunslinger wash');
-        }
-        else if (args[1].toLowerCase() === 'magician') {
-            message.channel.send('magician wash');
-        }
-        break;
-}*/
 
 bot.login(process.env.TOKEN);
