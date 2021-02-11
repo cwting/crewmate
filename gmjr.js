d661ec6edc8fc394929d8d68d6142a04a3cfddde
cweiting19 <cweiting.19@ichat.sp.edu.sg>
2021-02-11 12:50:51 +0800

    test allow others

--- gmjr.js
+++ gmjr.js
@@ -22,7 +22,7 @@ List of GMJr:
 bot.on('message', message => {
     // Nivi
     if (message.content.toLowerCase() === "c.gmjr-nivi") {
-        let gmjr = new Menu(message.channel, message.author.id, [
+        let gmjr = new Menu(message.channel, [
             {
                 name: 'Wolftail',
                 content: new MessageEmbed({
