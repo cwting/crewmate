8466b947a8f7267b05aec51d8be41e82b0e1c079
cwting <chenweiting02@gmail.com>
2021-02-13 00:35:26 +0800

    update spearmanwash

--- washes.js
+++ washes.js
@@ -4,6 +4,7 @@ const bot = new Discord.Client();
 var hpGain = 0;
 var mpLoss = 0;
 var vote = 8000;
+var aprNX = 3100;
 var aprCost = 12500000;
 
 bot.on("ready", () => {
@@ -24,18 +25,18 @@ bot.on('message', message => {
         }
         // HP gain from APR
         else if (args[0] === 'spearman' && (args[1] > 1 && args[1] <= 200) && (args[2] > 1 && args[2] <= 30000)) {
-            message.channel.send('spearman wash entered!');
-            console.log(`command: ${command}, args: ${args}, args[0]: ${args[0]}, args[1]: ${args[1]} , args[2]: ${args[2]}  `)
-            // var level = args[1];
-            // var cleanMP = arg[2];
-            // var hpGain = 49;
-            // var mpLoss = 4;
-            // var minMP = 148 + (level * 6);
-            // var extraMP = cleanMP - minMP
-            // var numOfWash = extraMP / mpLoss;
-            // var hpGained = numOfWash * hpGain;
+            //message.channel.send('spearman wash entered!');
 
-            // message.channel.send(`Your extra mp is ${extraMP}\nYou can wash ${numOfWash} times and gain an approximate of ${hpGained} HP.\n The cost of AP resets is: ${vote * numOfWash} NX.`)
+            var level = args[1];
+            var cleanMP = arg[2];
+            var hpGain = 49;
+            var mpLoss = 4;
+            var minMP = 148 + (level * 6);
+            var extraMP = cleanMP - minMP
+            var numOfWash = extraMP / mpLoss;
+            var hpGained = numOfWash * hpGain;
+
+            message.channel.send(`Your extra mp is ${extraMP}\nYou can wash ${numOfWash} times and gain an approximate of ${hpGained} HP.\n The cost of AP resets is: ${aprNX * numOfWash} NX. (${Math.ceil(aprNX * numOfWash / vote)} number of days of voting)`)
 
         }
         else if (args[0] === 'fighter') {
