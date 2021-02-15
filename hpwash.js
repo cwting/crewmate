5757648e44ac76f3421970c540d384a4d355a9d3
cwting <chenweiting02@gmail.com>
2021-02-16 00:44:15 +0800

    updated thief and magician hpwash

--- hpwash.js
+++ hpwash.js
@@ -80,14 +80,30 @@ bot.on('message', message => {
         }
         // thieves
         else if (args[0] === 'thief' && (args[1] > 1 && args[1] <= 200) && (args[2] > 1 && args[2] <= 30000)) {
-            var minHPGain = 20;
-            var avgHPGain = 22;
             var mpLoss = 12;
             var minMP = (14 * level) + 148;
             var extraMP = cleanMP - minMP
             var numOfWash = Math.floor(extraMP / mpLoss);
-            var minHPGained = numOfWash * minHPGain;
-            var avgHPGained = numOfWash * avgHPGain;
+            // apr
+            var minHPGainAPR = 16;
+            var avgHPGainAPR = 20;
+            var minHPGainedAPR = numOfWash * minHPGainAPR;
+            var avgHPGainedAPR = numOfWash * avgHPGainAPR;
+            // fresh
+            var minHPGainFresh = 20;
+            var avgHPGainFresh = 22;
+            var minHPGainedFresh = numOfWash * minHPGainFresh;
+            var avgHPGainedFresh = numOfWash * avgHPGainFresh;
+
+            return message.reply(
+                `You have ${extraMP.toLocaleString()} extra MP.\n` +
+                `You can wash ${numOfWash.toLocaleString()} times\n` +
+                `**If you use APRs:**\n` +
+                `You can gain at least ${Math.floor(minHPGainedAPR).toLocaleString()} HP and on average ${Math.floor(avgHPGainedAPR).toLocaleString()} HP.\n` +
+                `**If you use Fresh APs:**\n` +
+                `You can gain at least ${Math.floor(minHPGainedFresh).toLocaleString()} HP and on average ${Math.floor(avgHPGainedFresh).toLocaleString()} HP.\n` +
+                `The cost of AP resets is: ${Math.ceil(aprNX * numOfWash).toLocaleString()} NX(${Math.ceil(aprNX * numOfWash / vote).toLocaleString()} days of voting) or ${Math.ceil(aprMeso * numOfWash).toLocaleString()} mesos(${aprMeso.toLocaleString()} / AP Reset)`
+            )
         }
         // brawler
         else if (args[0] === 'brawler' && (args[1] > 1 && args[1] <= 200) && (args[2] > 1 && args[2] <= 30000)) {
@@ -112,8 +128,31 @@ bot.on('message', message => {
             var avgHPGained = Math.floor(numOfWash * avgHPGain);
         }
         // magicians
-        else if (args[0] === 'magician') {// && (args[1] > 1 && args[1] <= 200) && (args[2] > 1 && args[2] <= 30000)) {
-            return message.channel.send("i cannot do mage D:")
+        else if (args[0] === 'magician' && (args[1] > 1 && args[1] <= 200) && (args[2] > 1 && args[2] <= 30000)) {
+            var minHPGain = 10;
+            var avgHPGain = 15;
+            var mpLossS0 = 20;
+            var mpLossS10 = 30;
+            var minMP = (14 * level) + 148;
+            var extraMP = cleanMP - minMP
+            // Improved MapMP = 0
+            var numOfWashS0 = Math.floor(extraMP / mpLossS0);
+            var minHPGainedS0 = numOfWashS0 * minHPGainAPR;
+            var avgHPGainedS0 = numOfWashS0 * avgHPGainAPR;
+            // Improved MapMP = 10
+            var numOfWashS10 = Math.floor(extraMP / mpLossS10);
+            var minHPGainedS10 = numOfWashS10 * minHPGain;
+            var avgHPGainedS10 = numOfWashS10 * avgHPGain;
+
+            return message.reply(
+                `You have ${extraMP.toLocaleString()} extra MP.\n` +
+                `You can wash ${numOfWash.toLocaleString()} times\n` +
+                `**If Improved MaxMP Increase skill is level 0:**\n` +
+                `You can gain at least ${Math.floor(minHPGainedS0).toLocaleString()} HP and on average ${Math.floor(avgHPGainedS0).toLocaleString()} HP.\n` +
+                `**If Improved MaxMP Increase skill is level 10:**\n` +
+                `You can gain at least ${Math.floor(minHPGainedS10).toLocaleString()} HP and on average ${Math.floor(avgHPGainedS10).toLocaleString()} HP.\n` +
+                `The cost of AP resets is: ${Math.ceil(aprNX * numOfWash).toLocaleString()} NX(${Math.ceil(aprNX * numOfWash / vote).toLocaleString()} days of voting) or ${Math.ceil(aprMeso * numOfWash).toLocaleString()} mesos(${aprMeso.toLocaleString()} / AP Reset)`
+            )
         }
         else {
             return message.reply("Incorrect format!")
@@ -121,7 +160,8 @@ bot.on('message', message => {
         message.reply(
             `You have ${extraMP.toLocaleString()} extra MP.\n` +
             `You can wash ${numOfWash.toLocaleString()} times and gain at least ${Math.floor(minHPGained).toLocaleString()} HP and on average ${Math.floor(avgHPGained).toLocaleString()} HP.\n` +
-            `The cost of AP resets is: ${Math.ceil(aprNX * numOfWash).toLocaleString()} NX(${Math.ceil(aprNX * numOfWash / vote).toLocaleString()} days of voting) or ${Math.ceil(aprMeso * numOfWash).toLocaleString()} mesos(${aprMeso.toLocaleString()} / AP Reset)`)
+            `The cost of AP resets is: ${Math.ceil(aprNX * numOfWash).toLocaleString()} NX(${Math.ceil(aprNX * numOfWash / vote).toLocaleString()} days of voting) or ${Math.ceil(aprMeso * numOfWash).toLocaleString()} mesos(${aprMeso.toLocaleString()} / AP Reset)`
+        )
     }
 })
 bot.login(process.env.TOKEN);
