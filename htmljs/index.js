0dda47258fbcb08494ebe43882f1975fa809dd59
cweiting19 <cweiting.19@ichat.sp.edu.sg>
2021-03-05 03:56:12 +0800

    talk to npc button not working

--- htmljs/index.js
+++ htmljs/index.js
@@ -1,7 +1,12 @@
+var attemptNo = 1;
 $(document).ready(function () {
-    var count = 1;
     $('#btnStart').click(function () {
-        $('.game').append($(`
+        console.log("start btn clicked")
+        $(this).closest('#btnStart').remove();
+        $('.buttons').append($(`
+            <button type="button" id="btnTalkToNPC" class="btn btn-primary">Talk To NPC</button>
+        `));
+        $('.table-responsive').append(`
             <table class="table">
                 <thead>
                     <tr>
@@ -13,9 +18,9 @@ $(document).ready(function () {
                         <th scope="col">Feedback</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id="appender">
                     <tr>
-                        <td>${count}</th>
+                        <td>${attemptNo}</td>
                         <td>
                             <select id="statue1" class="form-select">
                             <option value="scroll" selected>Scroll</option>
@@ -25,7 +30,7 @@ $(document).ready(function () {
                             </select>
                         </td>
                         <td>
-                            <select id="statue1" class="form-select">
+                            <select id="statue2" class="form-select">
                             <option value="scroll" selected>Scroll</option>
                             <option value="medal">Medal</option>
                             <option value="wine">Wine</option>
@@ -33,7 +38,7 @@ $(document).ready(function () {
                             </select>
                         </td>
                         <td>
-                            <select id="statue1" class="form-select">
+                            <select id="statue3" class="form-select">
                             <option value="scroll" selected>Scroll</option>
                             <option value="medal">Medal</option>
                             <option value="wine">Wine</option>
@@ -41,22 +46,73 @@ $(document).ready(function () {
                             </select>
                         </td>
                         <td>
-                            <select id="statue1" class="form-select">
+                            <select id="statue4" class="form-select">
                             <option value="scroll" selected>Scroll</option>
                             <option value="medal">Medal</option>
                             <option value="wine">Wine</option>
                             <option value="food">Food</option>
                             </select>    
                         </td>
-                        <td>Feedback HEEERREE</td>
+                        <td>
+                            <textarea class="form-control" id="npcFB" rows="2" readonly></textarea>
+                        </td>
                     </tr>
                 </tbody>
             </table>
-        `));
-        count++;
+        `);
+        attemptNo++;
     });
-});
 
-$(document).ready(function () {
-    $(".dropdown-toggle").dropdown();
+    $('#btnTalkToNPC').click(function () {
+        console.log("npc btn clicked")
+        $('#appender').append(`
+            <tr>
+                <td>${attemptNo}</td>
+                <td>
+                    <select id="statue1" class="form-select">
+                    <option value="scroll" selected>Scroll</option>
+                    <option value="medal">Medal</option>
+                    <option value="wine">Wine</option>
+                    <option value="food">Food</option>
+                    </select>
+                </td>
+                <td>
+                    <select id="statue2" class="form-select">
+                    <option value="scroll" selected>Scroll</option>
+                    <option value="medal">Medal</option>
+                    <option value="wine">Wine</option>
+                    <option value="food">Food</option>
+                    </select>
+                </td>
+                <td>
+                    <select id="statue3" class="form-select">
+                    <option value="scroll" selected>Scroll</option>
+                    <option value="medal">Medal</option>
+                    <option value="wine">Wine</option>
+                    <option value="food">Food</option>
+                    </select>
+                </td>
+                <td>
+                    <select id="statue4" class="form-select">
+                    <option value="scroll" selected>Scroll</option>
+                    <option value="medal">Medal</option>
+                    <option value="wine">Wine</option>
+                    <option value="food">Food</option>
+                    </select>    
+                </td>
+                <td>
+                    <textarea class="form-control" id="npcFB" rows="2" readonly></textarea>
+                </td>
+            </tr>
+        `);
+        attemptNo++;
+    });
+
+    // ingredient drop down toggle
+    $(document).ready(function () {
+        $(".dropdown-toggle").dropdown();
+    });
+
+
+
 });
