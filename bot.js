82dd3b2259d719e311185007e2034640cc5208c0
cweiting19 <cweiting.19@ichat.sp.edu.sg>
2021-05-18 04:15:09 +0800

    update cron

--- bot.js
+++ bot.js
@@ -15,9 +15,15 @@ bot.on("ready", () => {
     bot.user.setActivity("c.help") // set BOT's status
 
     const eventCh = bot.channels.cache.get('828527080110424074');
-    const job = new cron.CronJob('00 00 */4 * * *', () => {
-        eventCh.send(`<@&${828282848724516875}>, time for cake!`);
-    });
+    const job = new cron.CronJob(
+        '00 55 */3 * * *',
+        '00 55 */7 * * *',
+        '00 55 */11 * * *',
+        '00 55 */15 * * *',
+        '00 55 */19 * * *',
+        '00 55 */23 * * *', () => {
+            eventCh.send(`<@&${"828282848724516875"}>, cake in 5 minutes!`);
+        });
 
     job.start()
 });
