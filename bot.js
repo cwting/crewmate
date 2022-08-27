c666a0b39aa35b16d385ce771214469971dba3c8
cwting <78128212+cwting@users.noreply.github.com>
2022-08-27 16:17:10 +0800

    check server reverted

--- bot.js
+++ bot.js
@@ -18,11 +18,6 @@ bot.on('ready', () => {
   console.info(`${bot.user.tag} is now online! (index.js)`); // BOT online
   bot.user.setActivity('c.help') // set BOT's status
 
-  bot.guilds.cache.forEach(guild => {
-    console.log(`${guild.name} | ${guild.id}`);
-    console.log(`Checking Servers...`)
-  })
-
   /* CRONJOB TIMEZONE = UTC */
   /* ------------------------------- Channels ------------------------------- */
 
