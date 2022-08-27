cb59ac9f6fa6debf35baa494f18adb9223413051
cwting <78128212+cwting@users.noreply.github.com>
2022-08-27 16:05:33 +0800

    check servers

--- bot.js
+++ bot.js
@@ -12,6 +12,10 @@ const cron = require('cron');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+bot.guilds.cache.forEach(guild => {
+  console.log(`${guild.name} | ${guild.id}`);
+})
+
 bot.on('ready', () => {
   console.info(`${bot.user.tag} is now online! (index.js)`); // BOT online
   bot.user.setActivity('c.help') // set BOT's status
